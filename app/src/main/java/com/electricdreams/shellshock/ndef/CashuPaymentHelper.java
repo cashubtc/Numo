52d439705fc8d08720556efb700e87e21cf46728
lollerfirst <lollerfirst@gmail.com>
2025-11-23 15:29:08 +0100

    Cleanup: remove Java stub for CashuPaymentHelper

--- app/src/main/java/com/electricdreams/shellshock/ndef/CashuPaymentHelper.java
+++
@@ -1,4 +0,0 @@
-package com.electricdreams.shellshock.ndef;
-
-// Kotlin migration: this Java class has been replaced by CashuPaymentHelper.kt
-// Intentionally left empty to avoid duplicate type definitions.
