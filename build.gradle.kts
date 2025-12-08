8261395a75ccd0c3b0cef86b82ccf4e8a538ca87
lollerfirst <lollerfirst@gmail.com>
2025-12-08 11:12:30 +0100

    fifx buildscript and plugins dependencies downgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.11.2")
+        classpath("com.android.tools.build:gradle:8.13.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.11.2" apply false
-    id("com.android.library") version "8.11.2" apply false
+    id("com.android.application") version "8.13.1" apply false
+    id("com.android.library") version "8.13.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
 }
