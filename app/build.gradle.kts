a9115df8ef0a6c90b9ef9d21e66496c1cb1a0316
lollerfirst <lollerfirst@gmail.com>
2025-12-15 12:31:46 +0100

    update cdk dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,7 +109,7 @@ dependencies {
     implementation("com.google.zxing:core:3.5.3")
 
     // CDK Kotlin bindings
-    implementation("org.cashudevkit:cdk-kotlin:0.14.2")
+    implementation("org.cashudevkit:cdk-kotlin:0.14.3-rc.0")
     
     // ML Kit Barcode Scanning
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
