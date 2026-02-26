397d403ec73cd9e8b6c42e866ad435686879b660
d4rp4t <d4rp4t@tutanota.com>
2026-02-26 12:27:38 +0100

    change test connection uri to require read_invoice permission

--- app/src/main/java/com/electricdreams/numo/feature/settings/BtcPaySettingsActivity.kt
+++ app/src/main/java/com/electricdreams/numo/feature/settings/BtcPaySettingsActivity.kt
@@ -109,10 +109,8 @@ class BtcPaySettingsActivity : AppCompatActivity() {
                         .readTimeout(10, TimeUnit.SECONDS)
                         .build()
 
-                    //todo check which permissions does it require.
-                    // probably shouldn't be used for hearbeat check
                     val request = Request.Builder()
-                        .url("$serverUrl/api/v1/stores/$storeId")
+                        .url("$serverUrl/api/v1/stores/$storeId/invoices")
                         .header("Authorization", "token $apiKey")
                         .get()
                         .build()
