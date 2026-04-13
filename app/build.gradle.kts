aac941887cdd412d35769256355dc629f321e50a
a1denvalu3 <cloudsupper@proton.me>
2026-04-13 20:04:32 +0200

    bump versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.electricdreams.numo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
