a69d255748dae8a30edebd58f820b665e5758869
a1denvalu3 <43107113+a1denvalu3@users.noreply.github.com>
2026-04-13 15:40:52 +0200

    chore: bump build number to 6 (#257)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.electricdreams.numo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
