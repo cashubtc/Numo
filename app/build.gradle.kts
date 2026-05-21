b433a1906add26f3e3002650e498e2de9f6be30c
a1denvalu3 <43107113+a1denvalu3@users.noreply.github.com>
2026-05-22 00:57:03 +0200

    Update build.gradle.kts (#322)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.electricdreams.numo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.5"
+        versionCode = 16
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
